jedbrown/git-fat

commit 309f63097ad43b4b0c484345fa7b6c3c6916bac5

fix the pull case when using rsync with ssh options

git-fat
@@ -109,6 +109,23 @@ class GitFat(object):
         if remote is None:
             raise RuntimeError('No rsync.remote in %s' % cfgpath)
         return remote, ssh_port, ssh_user
+    def get_rsync_command(self,push):
+        (remote, ssh_port, ssh_user) = self.get_rsync()
+        if push:
+            self.verbose('Pushing to %s' % (remote))
+        else:
+            self.verbose('Pulling from %s' % (remote))
+
+        cmd = ['rsync', '--progress', '--ignore-existing', '--from0', '--files-from=-']
+        rshopts = ''
+        if ssh_user:
+            rshopts += ' -l ' + ssh_user
+        if ssh_port:
+            rshopts += ' -p ' + ssh_port
+        if rshopts:
+            cmd.append('--rsh=ssh' + rshopts)
+        cmd += [self.objdir + '/', remote + '/']
+        return cmd
     def revparse(self, revname):
         return subprocess.check_output(['git', 'rev-parse', revname]).strip()
     def encode_v1(self, digest, bytes):
@@ -277,18 +294,7 @@ class GitFat(object):
         # (includes history). Finer-grained pushing would be useful.
         pushall = '--all' in args
         files = self.referenced_objects(all=pushall) & self.catalog_objects()
-        (remote, ssh_port, ssh_user) = self.get_rsync()
-        self.verbose('Pushing to %s' % (remote))
-
-        cmd = ['rsync', '--progress', '--ignore-existing', '--from0', '--files-from=-']
-        rshopts = ''
-        if ssh_user:
-            rshopts += ' -l ' + ssh_user
-        if ssh_port:
-            rshopts += ' -p ' + ssh_port
-        if rshopts:
-            cmd.append('--rsh=ssh' + rshopts)
-        cmd += [self.objdir + '/', remote + '/']
+        cmd = self.get_rsync_command(push=True)
         p = subprocess.Popen(cmd, stdin=subprocess.PIPE)
         p.communicate(input='\x00'.join(files))
     def checkout(self, show_orphans=False):
@@ -321,8 +327,7 @@ class GitFat(object):
             if rev:
                 refargs['rev'] = rev
         files = self.referenced_objects(**refargs) - self.catalog_objects()
-        remote = self.get_rsync()
-        cmd = ['rsync', '--progress', '--ignore-existing', '--from0', '--files-from=-', remote + '/', self.objdir + '/']
+        cmd = self.get_rsync_command(push=False)
         p = subprocess.Popen(cmd, stdin=subprocess.PIPE)
         p.communicate(input='\x00'.join(files))
         self.checkout()

commit 6208687a0ba1f6c3e84630a42035cc6b7947d989

Fix broken rsync pull, bug introduced in 309f63097a

git-fat
@@ -124,7 +124,10 @@ class GitFat(object):
             rshopts += ' -p ' + ssh_port
         if rshopts:
             cmd.append('--rsh=ssh' + rshopts)
-        cmd += [self.objdir + '/', remote + '/']
+        if push:
+            cmd += [self.objdir + '/', remote + '/']
+        else:
+            cmd += [remote + '/', self.objdir + '/']
         return cmd
     def revparse(self, revname):
         return subprocess.check_output(['git', 'rev-parse', revname]).strip()
@@ -295,6 +298,7 @@ class GitFat(object):
         pushall = '--all' in args
         files = self.referenced_objects(all=pushall) & self.catalog_objects()
         cmd = self.get_rsync_command(push=True)
+        self.verbose('Executing: %s' % ' '.join(cmd))
         p = subprocess.Popen(cmd, stdin=subprocess.PIPE)
         p.communicate(input='\x00'.join(files))
     def checkout(self, show_orphans=False):
@@ -328,6 +332,7 @@ class GitFat(object):
                 refargs['rev'] = rev
         files = self.referenced_objects(**refargs) - self.catalog_objects()
         cmd = self.get_rsync_command(push=False)
+        self.verbose('Executing: %s' % ' '.join(cmd))
         p = subprocess.Popen(cmd, stdin=subprocess.PIPE)
         p.communicate(input='\x00'.join(files))
         self.checkout()

test-retroactive.sh
@@ -32,3 +32,19 @@ git checkout HEAD^
 rm *
 git checkout .
 ls -al
+
+# Set up place to push
+git checkout master
+cat > .gitfat <<EOF
+[rsync]
+remote = $(realpath ../retro-store)
+EOF
+git add .gitfat
+git commit -m'Add .gitfat for local push'
+git fat push
+
+cd ..
+git clone file:///$(realpath retro) retro-clone
+cd retro-clone
+git fat init
+git fat pull