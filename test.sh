e4eedb4f6117fb599d149962d2b0df45d9005814
Stephen Miller <stephen.l.miller1@navy.mil>
2013-03-22 09:29:59 -0700

    Expanded test script to check push and pull

--- test.sh
+++ test.sh
@@ -2,8 +2,21 @@
 # Any copyright is dedicated to the Public Domain.
 # http://creativecommons.org/publicdomain/zero/1.0/
 
-set -e
+# Clean up any existing directories from previous runs
+if [ -d "fat-test" ] 
+then
+	rm -rf fat-test
+fi
+if [ -d "fat-test2" ]
+then
+	rm -rf fat-test2
+fi
 
+# Enable verbose mode
+set -e
+export GIT_FAT_VERBOSE=1
+
+# Initialize first repo
 git init fat-test
 cd fat-test
 git fat init
@@ -14,7 +27,7 @@ EOF
 echo '*.fat filter=fat -crlf' > .gitattributes
 git add .gitattributes .gitfat
 git commit -m'Initial fat repository'
-
+# Add content to repo
 echo 'fat content a' > a.fat
 git add a.fat
 git commit -m'add a.fat'
@@ -23,3 +36,12 @@ git add b.fat
 git commit -m'add b.fat'
 echo 'revise fat content a' > a.fat
 git commit -am'revise a.fat'
+git fat push
+
+
+# Clone first repo into second
+cd ..
+git clone fat-test fat-test2
+cd fat-test2
+git fat init
+git fat pull
