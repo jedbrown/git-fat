0b85d5aec3a74c669229be1f3bcf03b103ac9f2e
Carlos Hernandez <carlos@hrndz.ca>
2023-04-28 10:31:12 -0600

    Update devshell

--- flake.nix
+++ flake.nix
@@ -8,21 +8,37 @@
     inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, flake-utils, poetry2nix }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        # see https://github.com/nix-community/poetry2nix/tree/master#api for more functions and examples.
-        inherit (poetry2nix.legacyPackages.${system}) mkPoetryApplication;
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
-        packages = {
-          git-fat = mkPoetryApplication { projectDir = self; };
-          default = self.packages.${system}.git-fat;
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    poetry2nix,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      # see https://github.com/nix-community/poetry2nix/tree/master#api for more functions and examples.
+      inherit (poetry2nix.legacyPackages.${system}) mkPoetryApplication mkPoetryEnv;
+      poetryEnv = mkPoetryEnv {
+        projectDir = ./.;
+        overrides = [
+          pkgs.poetry2nix.defaultPoetryOverrides
+        ];
+        editablePackageSources = {
+          git-fat = ./.;
         };
+      };
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      packages = {
+        git-fat = mkPoetryApplication {projectDir = self;};
+        default = self.packages.${system}.git-fat;
+      };
 
-        devShells.default = pkgs.mkShell {
-          packages = [ poetry2nix.packages.${system}.poetry ];
-        };
-      });
+      devShells.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          python3
+          poetry
+          poetryEnv
+        ];
+      };
+    });
 }
