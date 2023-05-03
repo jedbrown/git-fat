a112f92b67acbd867e30ab3b10c0d59e17514363
Carlos Hernandez <carlos@hrndz.ca>
2023-05-03 14:27:04 -0600

    Create bucket as part of test setup

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,8 +5,11 @@ from git.repo import Repo
 import requests
 from urllib3.util.retry import Retry
 from requests.adapters import HTTPAdapter
+import boto3
+import boto3.session
 
 pytest_plugins = ["docker_compose"]
+bucket_name = "munki-repo"
 
 
 class ClonedGitRepo(Workspace):
@@ -37,9 +40,9 @@ def test_s3_git_repo(git_repo, resource_path_root):
     s3_test_resources = resource_path_root / "s3"
     copy_files(str(s3_test_resources), str(path))
     git_fat_conf = path / ".gitfat"
-    conf = """
+    conf = f"""
 [s3]
-bucket = munki-repo
+bucket = {bucket_name}
 extrapushargs = --acl bucket-owner-full-control"""
     git_fat_conf.write_text(conf)
     git_repo.run("git fat init")
@@ -55,6 +58,21 @@ def test_s3_git_repo_clone(test_s3_git_repo):
     return repo
 
 
+def create_bucket(api_url):
+    s3 = boto3.resource(
+        "s3",
+        endpoint_url=api_url,
+        aws_access_key_id="root",
+        aws_secret_access_key="password",
+        aws_session_token=None,
+        config=boto3.session.Config(signature_version="s3v4"),
+        verify=False,
+    )
+    bucket = s3.Bucket(bucket_name)
+    if not bucket.creation_date:
+        s3.create_bucket(Bucket=bucket_name)
+
+
 # Invoking this fixture: 'function_scoped_container_getter' starts all services
 @pytest.fixture(scope="session")
 def wait_for_s3(session_scoped_container_getter):
@@ -64,6 +82,8 @@ def wait_for_s3(session_scoped_container_getter):
     request_session.mount("http://", HTTPAdapter(max_retries=retries))
 
     service = session_scoped_container_getter.get("minio").network_info[0]
-    api_url = "http://%s:%s/minio/health/live" % ("127.0.0.1", service.host_port)
-    assert request_session.get(api_url)
+    api_url = "http://%s:%s" % ("127.0.0.1", service.host_port)
+    health_check_url = f"{api_url}/minio/health/live"
+    create_bucket(api_url)
+    assert request_session.get(health_check_url)
     return request_session, api_url
