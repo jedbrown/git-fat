93e806a48aa3a58c2a7f5446e055928c64270ae0
Carlos Hernandez <carlos@hrndz.ca>
2023-05-05 07:56:12 -0600

    Add test for listing s3 fatstore

--- tests/s3/test_s3fatstore.py
+++ tests/s3/test_s3fatstore.py
@@ -1,4 +1,9 @@
 def test_upload_file(wait_for_s3, workspace, s3_fatstore):
-    test_file = (workspace.workspace / "test.txt")
+    test_file = workspace.workspace / "test.txt"
     test_file.write_text("Hello World\n")
     s3_fatstore.upload(test_file.abspath())
+
+
+def test_list(wait_for_s3, s3_fatstore):
+    files = s3_fatstore.list()
+    assert len(files) >= 1
